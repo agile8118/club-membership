4ad0ff4750be5a93ea3abe3b003ec20433b7d9d0
Joseph Heidari <agile.8272@gmail.com>
2024-04-04 23:50:06 -0400

    Adds a middleware function for authentication

--- app.py
+++ app.py
@@ -7,12 +7,37 @@ from flask import (
     redirect,
     url_for,
 )
+from DB import DB
+
+DB = DB()
 
 # Controllers
 from controllers.authentication import Authentication
 
 
 app = Flask(__name__)
+
+
+# Middleware to check if user is authenticated
+@app.before_request
+def check_authentication():
+    non_protected_routes = ["/login", "/register", "/"]
+    if request.path not in non_protected_routes and not request.path.startswith(
+        "/static"
+    ):
+        auth_token = request.headers.get("Authorization")
+
+        if not auth_token:
+            return jsonify({"error": "Unauthorized"}), 401
+
+        # Check if token is valid
+        DB.update()
+        for session in DB.sessions:
+            if session["token"] == auth_token:
+                request.user_id = session["user_id"]
+                return
+
+        return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.route("/")
@@ -30,6 +55,12 @@ def login():
 def register():
     if request.method == "POST":
         return Authentication.register()
+
+
+@app.route("/protected-test", methods=["GET"])
+def pt():
+    if request.method == "GET":
+        return jsonify({"user": request.user_id})
 
 
 if __name__ == "__main__":
--- controllers/authentication.py
+++ controllers/authentication.py
@@ -1,5 +1,7 @@
 from flask import request, jsonify
 import bcrypt
+import random
+import base64
 from DB import DB
 
 DB = DB()
@@ -12,11 +14,29 @@ class Authentication:
         phone = request.json.get("phone")
         password = request.json.get("password")
 
+        DB.update()
+
         # perform authentication here
         for user in DB.users:
             if user["phone"] == phone:
-                if bcrypt.checkpw(password.encode("utf-8"), user["password"]):
-                    return jsonify({"message": "Login successful"})
+                hashed_password_base64 = user["password"]
+                hashed_password = base64.b64decode(
+                    hashed_password_base64.encode("utf-8")
+                )
+
+                if bcrypt.checkpw(password.encode("utf-8"), hashed_password):
+                    # Generate a random token
+                    token = str(random.randint(0, 10000000000))
+
+                    # remove the previous session token if it exists
+                    for session in DB.sessions:
+                        if session["user_id"] == user.get("id"):
+                            DB.sessions.remove(session)
+
+                    DB.sessions.append({"token": token, "user_id": user.get("id")})
+                    DB.save()
+
+                    return jsonify({"message": "Login successful", "token": token})
 
         # return error message if authentication fails
         return jsonify({"error": "Invalid phone or password"})
@@ -31,8 +51,8 @@ class Authentication:
 
         # hash the provided password
         hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-
-        print(DB.users[0]["name"])
+        # convert the hashed password to base64 to be able to save to a file as JSON
+        hashed_password_base64 = base64.b64encode(hashed_password).decode("utf-8")
 
         # check if phone and email not taken
         for user in DB.users:
@@ -42,18 +62,22 @@ class Authentication:
                 return jsonify({"error": "Email already taken"})
 
         # add user to DB
-        DB.users.append(
-            {
-                "id": len(DB.users) + 1,
-                "name": name,
-                "phone": phone,
-                "email": email,
-                "address": address,
-                "password": hashed_password,
-            }
-        )
+        user = {
+            "id": len(DB.users) + 1,
+            "name": name,
+            "phone": phone,
+            "email": email,
+            "address": address,
+            "password": hashed_password_base64,
+        }
+        print(hashed_password_base64)
+        DB.users.append(user)
+
+        # Generate a random token
+        token = str(random.randint(0, 10000000000))
+        DB.sessions.append({"token": token, "user_id": user.get("id")})
 
         # save DB
         DB.save()
 
-        return "HEY"
+        return jsonify({"message": "User registered successfully.", "token": token})
