d7825d7bb350de590c57c23d2e0032b24c416b86
Joseph Heidari <agile.8272@gmail.com>
2024-04-10 17:48:46 -0400

    Updates the database to support the practices table

--- DB.py
+++ DB.py
@@ -5,12 +5,17 @@ class DB:
     def __init__(self):
         self.users_path = "./data/users"
         self.sessions_path = "./data/sessions"
+        self.practices_path = "./data/practices"
 
         with open(self.users_path, "r") as users_file:
             self.users = json.load(users_file)
 
         with open(self.sessions_path, "r") as sessions_file:
             self.sessions = json.load(sessions_file)
+
+        with open(self.practices_path, "r") as practices_file:
+            self.practices = json.load(practices_file)
+
 
     def update(self):
         with open(self.users_path, "r") as users_file:
@@ -19,9 +24,17 @@ class DB:
         with open(self.sessions_path, "r") as sessions_file:
             self.sessions = json.load(sessions_file)
 
+        with open(self.practices_path, "r") as practices_file:
+            self.practices = json.load(practices_file)
+
+
     def save(self):
         with open(self.users_path, "w") as users_file:
             json.dump(self.users, users_file)
 
         with open(self.sessions_path, "w") as sessions_file:
             json.dump(self.sessions, sessions_file)
+
+
+        with open(self.practices_path, "w") as practices_file:
+            json.dump(self.practices, practices_file)
